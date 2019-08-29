7333fcfb5d70a33f7812722402aa458b606b4224
luthfiazri <luthfiazri@gmail.com>
2019-08-29 15:27:26 +0700

    penamban file fungtion dan return

---
+++ fungtion.js
@@ -0,0 +1,17 @@
+function jalanSatu() {
+  console.log("Jalan Satu");
+}
+
+const jalanDua = function () {
+  console.log("Jalan Dua");
+}
+
+jalanSatu();
+jalanDua();
+
+
+const jalanTiga = ()=>{
+  console.log("Jalan Tiga");
+}
+
+jalanTiga();
